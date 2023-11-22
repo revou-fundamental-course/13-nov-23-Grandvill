4665debb4e863552332be0e2cfe5658ee385d700
unknown <zahidan23@gmail.com>
2023-11-22 13:06:07 +0700

    Final

--- js/script.js
+++ js/script.js
@@ -13,3 +13,21 @@ function displayImages() {
   images[index - 1].style.display = 'block';
   setTimeout(displayImages, 2000);
 }
+
+function validateForm() {
+  let x = document.forms['message-us']['name'].value;
+  if (x == '') {
+    alert('Name must be filled out');
+    return false;
+  }
+  let y = document.forms['message-us']['email'].value;
+  if (y == '') {
+    alert('Email must be filled out');
+    return false;
+  }
+  let z = document.forms['message-us']['option'].value;
+  if (z == '') {
+    alert('option must be filled out');
+    return false;
+  }
+}
